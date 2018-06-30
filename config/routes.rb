d8b4abd5c8a54b26a8ca83f63ef62a80f869a6d0
Yuezhi Li <clivialee@gmail.com>
2018-06-30 16:02:14 +0800

    _

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      post '/login', to: "login#login"
       resources :fields, only: [:index, :show, :create, :update, :destroy] do
         resources :hours, only: [:create, :destroy]
         resources :bookings, only: [:create]
