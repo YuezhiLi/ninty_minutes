f72e5485f4317181a1eb0bda256eebbf7044b697
Yuezhi Li <clivialee@gmail.com>
2018-07-05 00:02:18 +0800

    -

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ProfilesController < Api::V1::BaseController
   def show
     @user = @current_user
-    reservations = @user.reservations.map { |r| {field: r.field, people: r.users.count, start_time: r.start_time, end_time: r.end_time, confirmed: r.confirmed} }
+    reservations = @user.reservations.map { |r| {field: r.field, people: r.users.count, start_time: r.start_time.in_time_zone('Beijing'), end_time: r.end_time.in_time_zone('Beijing'), confirmed: r.confirmed} }
     @confirmed_reservations = reservations.select { |r| r[:confirmed] }
     @unconfirmed_reservations = reservations.select { |r| r[:confirmed] == false }
   end
